b510c43458405f16b38d8e1dced7889cd052d73a
Mahdyaralipor <mahdyaralipor0@gmail.com>
2025-03-09 11:38:24 +0330

    add channels log

--- config/logging.php
+++ config/logging.php
@@ -126,12 +126,12 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+
         'query' => [
-        'driver' => 'single',
-        'path' => storage_path('logs/query.log'),
-        'level' => 'info',
+            'driver' => 'single',
+            'path' => storage_path('logs/query.log'),
+            'level' => 'info',
+        ],
     ],
-
-    ],
-
+    
 ];
